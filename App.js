05d02ce0734d5e018e03c69cdf6419f77fe88d71
Samantha <samanthakamath2000@gmail.com>
2020-09-28 04:21:09 -0400

    Added ReadMe

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity, ScrollView, Button } from 'react-native';
 
 const styles = StyleSheet.create({
   heading: {
+    flex: 1,
     color: 'black',
     textAlign: 'center',
     fontSize: 30,
@@ -26,13 +27,14 @@ const styles = StyleSheet.create({
     alignSelf: 'stretch',
     textDecorationLine: 'line-through'
   },
-  // container: {
-  //   flex: 1,
-  //   backgroundColor: 'skyblue'
-  // },
-  listBackground: {
+  container: {
     flex: 1,
-    backgroundColor: 'white'
+    backgroundColor: 'blue'
+  },
+  listBackground: {
+    flex: 15,
+    backgroundColor: 'white',
+    height: '20px'
   },
   checkbox: {
     width: 15,
@@ -44,6 +46,11 @@ const styles = StyleSheet.create({
     height: 17,
     margin: 4
   },
+  cross: {
+    width: 15,
+    height: 15,
+    margin: 5
+  },
   plus: {
     width: 15,
     height: 15,
@@ -51,16 +58,17 @@ const styles = StyleSheet.create({
   },
   plusBackground: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: 'skyblue',
     alignItems: 'flex-end',
-    top: '410px'
+    justifyContent: 'flex-end',
+    paddingRight: '3px',
+    paddingBottom: '2px'
   }
 });
 
-
 class ToDoItem extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       checked: false,
       input: false
@@ -71,7 +79,12 @@ class ToDoItem extends Component {
     if (this.state.input) {
       this.setState({checked : !this.state.checked});
     }
- }
+  }
+
+
+  RemoveItem = (key) => {
+    this.props.data = this.props.data.filter( (data) => data.key !== key );
+  }
 
   render() {
     let circle = {
@@ -80,39 +93,78 @@ class ToDoItem extends Component {
     let check = {
       uri: "https://visualpharm.com/assets/752/Checked-595b40b65ba036ed117d3d93.svg"
     }
+    let cross = {
+      uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcReLjziNNzVvFt9Yc2Pi_BvoPrfQhHfZWw--A&usqp=CAU"
+    }
     return (
-          <TouchableOpacity style = {{flexDirection: 'row'}} onPress = {this.ToggleCheckbox}>
-            <View>
-              {this.state.checked && this.state.input ? (<Image source={check} style = {styles.check}/>) : (<Image source = {circle} style = {styles.checkbox} />) }
-            </View>
-            <View>
-              <TextInput style = {this.state.checked && this.state.input ? styles.done : styles.toDo} placeholder = 'Enter item here' multiline = {true} onChangeText = {(text) => text == "" ? this.setState({input: false}) : this.setState({input: true})} />
-            </View>
-          </TouchableOpacity>
+      <View style = {{flexDirection: 'row'}}>
+        <TouchableOpacity style = {{flexDirection: 'row'}} onPress = {this.ToggleCheckbox}>
+          <View>
+            {this.state.checked && this.state.input ? (<Image source={check} style = {styles.check}/>) : (<Image source = {circle} style = {styles.checkbox} />) }
+          </View>
+          <View>
+            <TextInput style = {this.state.checked && this.state.input ? styles.done : styles.toDo} placeholder = 'Enter item here' multiline = {true} onChangeText = {(text) => text == "" ? this.setState({input: false}) : this.setState({input: true})} />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity style = {{position: 'absolute', left: '290px'}} >
+            <Image source = {cross} style = {styles.cross}/>
+        </TouchableOpacity>
+      </View>
     )
   }
 }
 
-export default class ToDoList extends Component {
+class ToDoList extends Component {
+  constructor(props) {
+   super(props);
+   this.state = {    /* initial your state. without any added component */
+      data: [{key: 0}],
+      keys: []
+   }
+  }
+
+  AddItem = () => {
+    let added_data = {
+      key: this.state.data[this.state.data.length-1].key+1
+    };
+    this.setState({
+        data: [...this.state.data, added_data]
+    });
+  }
   render() {
     let plus = {
       uri: "https://image.flaticon.com/icons/svg/32/32339.svg"
     };
+    let addedItems = this.state.data.map( (data, index) => {
+        return (
+            <ToDoItem key = {data.key} data = {this.state.data}/>
+        )
+    });
     return (
-      <View>
-        <View>
-          <TextInput style = {styles.heading} placeholder = 'Heading Here' />
-        </View>
+      <View style = {styles.container}> 
+        <TextInput style = {styles.heading} placeholder = 'Heading Here' />
         <View style={styles.listBackground}>
-          <ToDoItem/>
-          <ToDoItem/>
-        </View>
+          <ScrollView>
+            {addedItems}
+          </ScrollView>
+        </View>        
         <View style = {styles.plusBackground}>
-          <TouchableOpacity onPress = {React.createElement(ToDoItem)}>
+          <TouchableOpacity onPress = {this.AddItem}>
             <Image source = {plus} style = {styles.plus}/>
           </TouchableOpacity>
         </View>
       </View>
+    );
+  }
+}
+
+export default class App extends Component {
+  constructor(props) {
+   super(props);
+  }
+  render() {
+    return (
+        <ToDoList/>
     );
   }
 }
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 ToDoApp
+
+This is a React Native App allowing users to create customizable To Do Lists. The app can be run on iOS and Android devices.
+
+Installation/Build
+
+To run and debug onto an iOS device, users can use Expo CLI with a recent version of Node.js. Install the Expo CLI app onto your phone and connect to the same wifi as your computer. Run the following commands in Terminal to start the development server:
+
+cd ToDoApp
+npm start
+
+For Android, use the Expo App scanner to scan the QR code and run the app. For iOS, use the device camera to do the same. 
+
+Usage
+
+After opening the app, the user can customize their To Do List with their header and To Do items. Users can check, add, and delete items as needed.
+
+Support 
+
+You can reach me at samanthakamath2000@gmail.com for questions.
+
+
+Project Status
+
+This project is mid-development.
+
+
+
